be850e2d8f0736385c395f921a10057b48c2274d
andcebrod <acrmontellano@gmail.com>
2017-11-08 18:24:10 +0100

    ejercicio get y post terminado

--- andcebrod/Ejercicios/get_y_post/editar_alumnos.php
+++ andcebrod/Ejercicios/get_y_post/editar_alumnos.php
@@ -9,19 +9,21 @@
     <form method="post">
       <fieldset>
         <?php if (!isset($_POST['email'])) : ?>
+
         <?php
-        echo "<span>Nombre: <input type='text' name='nombre' value='".$_GET['nombre']."'></span>"
-        echo "<span>Apellidos: <input type='text' name='apellidos' value='".$_GET['apellidos']."'></span>"
-        echo "<span>Email: <input type='text' name='email' value='".$_GET['email']."'></span>"
+        echo "<span>Nombre: <input type='text' name='nombre' value='".$_GET['nombre']."'></span>";
+        echo "<span>Apellidos: <input type='text' name='apellidos' value='".$_GET['apellidos']."'></span>";
+        echo "<span>Email: <input type='text' name='email' value='".$_GET['email']."'></span>";
         echo "<span><input type='submit' name='submit' value='submit'><span>";
          ?>
+
        <?php else: ?>
 
          <?php
 
-         echo "<p>Nuevos Datos: ".$_POST['nombre'] "</p>";
-         echo "<p>Nuevos Datos: ".$_POST['apellidos'] "</p>";
-         echo "<p>Nuevos Datos: ".$_POST['email'] "</p>";
+         echo "<p>Nuevos Datos: ".$_POST['nombre']."</p>";
+         echo "<p>Nuevos Datos: ".$_POST['apellidos']."</p>";
+         echo "<p>Nuevos Datos: ".$_POST['email']."</p>";
 
 
           ?>
